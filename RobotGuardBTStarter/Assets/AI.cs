800d45fa7595cf3b40fc396b08a9ba6bace18163
sigor1202 <sigor7829@gmail.com>
2021-05-31 19:00:40 -0300

    boatAi
    aula 27/05

--- RobotGuardBTStarter/Assets/AI.cs
+++ RobotGuardBTStarter/Assets/AI.cs
@@ -44,15 +44,18 @@ public class AI : MonoBehaviour
     //faz o personagem recuperar vida se ficar menor que 100
     void UpdateHealth()
     {
+        //se a vida for menor que 100
        if(health < 100)
+            //adiciona 1 a vida
         health ++;
     }
 
     //se colidir e a tag for bullet
     void OnCollisionEnter(Collision col)
-    {
+    {   //se colidir com um gameObject com a tag bullet
         if(col.gameObject.tag == "bullet")
         {
+            //subtrai 10 da vida
             health -= 10;
         }
     }
@@ -60,13 +63,16 @@ public class AI : MonoBehaviour
     //randomiza o destino
     public void PickRandomDestination()
     {
+        //randomiza os valores de x e zentre -100 e 100
         Vector3 dest = new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100));
+        //seta o destino com os valores de dest
         agent.SetDestination(dest);
         Task.current.Succeed(); 
     }
     [Task]
     //move para o destino
     public void MoveToDestination()
+
     { if (Task.isInspected) Task.current.debugInfo = string.Format("t={0:0.00}", Time.time);
         if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
         {
@@ -74,115 +80,126 @@ public class AI : MonoBehaviour
         }
     }
 
-    //cria o metodo para ir para o destino para fazer a patrulha
+  
     [Task]
     public void PickDestination(int x, int z)
     {
-        //Cria a variavel para pegar as posições dos eixos x e z
+        //coloca o valor de x e z no vetor
         Vector3 dest = new Vector3(x, 0, z);
-        //Seta o destino do NPC para a variavel dest
+        //seta o destino com os valores do velor dest
         agent.SetDestination(dest);
-        //fala que a ação foi concluida com sucesso
+
         Task.current.Succeed();
     }
-    //*Fim Patroll.BT
-
-    //*Attack.BT
+   
     [Task]
-    //metodo boolenao para olhar para o player
+   
     bool SeePlayer()
     {
-        //fala que a distancia dele vai ser a distancia dele menos a do player
+       //calcula a distancia entre o robo e o player
         Vector3 distance = player.transform.position - this.transform.position;
-        //cria o raycast
+        //cria o RayCast hit
         RaycastHit hit;
-        //booleano para ver a parede
+        //cria uma variavel do tipo bool e seta como falsa 
         bool seeWall = false;
-        //debug para desenhar o raycast
+        //desnha uma linha vermelha
         Debug.DrawRay(this.transform.position, distance, Color.red);
-        //se o raycast tocar
+       //se o ray cast detectar algo
         if (Physics.Raycast(this.transform.position, distance, out hit))
         {
-            //se o raycast colidir com um objeto com a tag wall seeWall fica true
+            //se a tag do objeto etectado for igual a wall
             if (hit.collider.gameObject.tag == "wall")
             {
-                //fica true
+                //seta a variavel como true
                 seeWall = true;
             }
         }
-        //se estiver rodando a aplicação
+        //retorna o valor de see wallcomo debug
         if (Task.isInspected)
         {
-            //mostra a distancia da wall
             Task.current.debugInfo = string.Format("wall={0}", seeWall);
         }
-        //se a distancia dor menor que a distancia de visibilidade e caso seewall seja false retorne como true
+        //se a distancia for menor que o raoi de visão e seeWall for falso
         if (distance.magnitude < visibleRange && !seeWall)
         {
-            //retorna como true
+            //retorna true
             return true;
         }
-        //caso não retorne como falso
+        //se não retorna falso
         else
         {
-            //retorna como false
             return false;
         }
     }
     [Task]
-    //metodo para ver o player
     public void TargetPlayer()
     {
-        //pega a posição do player
+        //pega o transform do player como target
         target = player.transform.position;
-        //fala que a ação foi concluida com sucesso
+        //task completa
         Task.current.Succeed();
     }
 
     [Task]
-    //olha para o target
+    
     public void LookAtTarget()
     {
-        //cria a variavel para detectar a direção sendo menos que a posição do player
+        //calcula a direção
         Vector3 direction = target - this.transform.position;
-        //faz a rotação ser mais suave
+        //rotaciona
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
-        //se estiver rodando a aplicação
+        //se estiver rodando
         if (Task.isInspected)
         {
-            //mostra o angle com debug
+            //mostra o angulo como debug e olha para ele
             Task.current.debugInfo = string.Format("angle={0}", Vector3.Angle(this.transform.forward, direction));
         }
-        //se for menor que 5 complete a task
+        //se a direferença do angulo for menor que 5 completa a tesk
         if (Vector3.Angle(this.transform.forward, direction) < 5.0f)
         {
-            //fala que a ação foi concluida com sucesso
             Task.current.Succeed();
         }
     }
-    //atualização
+
     [Task]
-    
     public bool Fire()
     {
-        //estancia o bulletPrefab e o gruarda na variavel bullet
+        //instamcia o prefab da bala e guarda na variavel bullet
         GameObject bullet = GameObject.Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
-        //adiciona força na variavel bullet
+        //adiciona força na bala
         bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward * 2000);
-
+        //retorna true
         return true;
     }
  
 
   
     [Task]
-
+    //faz ele virar no angulo escolhido
     bool Turn(float angle)
     {
-        //faz o robo rotacionar
+        
         var p = this.transform.position + Quaternion.AngleAxis(angle, Vector3.up) * this.transform.forward;
-       //alco igual a variavel utilizada na rotação
         target = p;
+        return true;
+    }
+
+    [Task]
+    public bool IsHealthLessThan(float health)
+    {
+        //compara a vida com o valor escolhido no botAi e retorna esse valor 
+        return this.health < health;
+    }
+
+    
+    [Task]
+    public bool Explode()
+    {
+        //destroi a barra de vida
+        Destroy(healthBar.gameObject);
+        //destroi a si mesmo
+        Destroy(this.gameObject);
+        //retorna true
         return true;
     }
 }
